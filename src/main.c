6d519ea62d18b1538cba292d63601f3c9816556c
Tinchetsu <tinchetsu@gmail.com>
2021-08-04 01:15:13 -0300

    changes

--- src/main.c
+++ src/main.c
@@ -1,21 +1,5 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window (adapted for HTML5 platform)
-*
-*   This example is prepared to compile for PLATFORM_WEB, PLATFORM_DESKTOP and PLATFORM_RPI
-*   As you will notice, code structure is slightly diferent to the other examples...
-*   To compile it for PLATFORM_WEB just uncomment #define PLATFORM_WEB at beginning
-*
-*   This example has been created using raylib 1.3 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2015 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
 #include "engine.h"
-//#define PLATFORM_WEB
 
 #if defined(PLATFORM_WEB)
     #include <emscripten/emscripten.h>
@@ -30,21 +14,19 @@ Texture2D texture;
 Font font;
 Map* map;
 
-//----------------------------------------------------------------------------------
-// Module Functions Declaration
-//----------------------------------------------------------------------------------
 void UpdateDrawFrame(void);     // Update and Draw one frame
 
-//----------------------------------------------------------------------------------
-// Main Enry Point
-//----------------------------------------------------------------------------------
+void initApp() {
+    App* app = getApp();
+    app->width = GetScreenWidth();
+    app->height = GetScreenHeight();
+}
+
 int main()
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
-    //tic-80-wide-font.ttf
+    
+    initApp();
     
     texture = LoadTexture("resources/tiles.png");
     font = LoadFontEx("resources/tic-computer-6x6-font.ttf", 6, 0, 0);
@@ -58,26 +40,17 @@ int main()
     emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
 #else
     SetTargetFPS(60);   // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         UpdateDrawFrame();
     }
 #endif
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
     CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
 
     return 0;
 }
 
-//----------------------------------------------------------------------------------
-// Module Functions Definition
-//----------------------------------------------------------------------------------
 void UpdateDrawFrame(void)
 {
     // Update
